b2138af7cddab1b6fc5f924ed1b63afc04b382df
juyeong <juyeongno1@naver.com>
2022-08-16 22:06:57 +0900

    시작뷰

--- SharingMyWishlist/SharingMyWishlist/ViewController.swift
+++ SharingMyWishlist/SharingMyWishlist/ViewController.swift
@@ -1,12 +1,19 @@
+
 import UIKit
-import Then
-import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    override func viewDidAppear(_ animated: Bool) {
+        let vc = LoginViewController()
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
+    }
+
+
 }
 
