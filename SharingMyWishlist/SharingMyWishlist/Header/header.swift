199008aa13acdbd630f0031667486d1a870e82a5
BJCHO0501 <bjcho1503@naver.com>
2022-08-23 22:06:57 +0900

    해더 & 클라이언트 추가

---
+++ SharingMyWishlist/SharingMyWishlist/Header/header.swift
@@ -0,0 +1,8 @@
+//
+//  header.swift
+//  SharingMyWishlist
+//
+//  Created by 조병진 on 2022/08/23.
+//
+
+import Foundation
---
+++ SharingMyWishlist/SharingMyWishlist/HttpClient/Client.swift
@@ -0,0 +1,8 @@
+//
+//  Client.swift
+//  SharingMyWishlist
+//
+//  Created by 조병진 on 2022/08/23.
+//
+
+import Foundation
